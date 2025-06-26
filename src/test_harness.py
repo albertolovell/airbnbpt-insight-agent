57b7f9f4a033ea515be6166e4ff3f0fd081f32a0
albertolovell <alberto.lovell@gmail.com>
2025-06-25 18:30:29 -0700

    get_point -> retrieve

--- src/test_harness.py
+++ src/test_harness.py
@@ -1,8 +1,7 @@
 from agent_core import run_agent, vector_store, query_neo4j
+from qdrant_client import QdrantClient
 
 def main():
-
-
 
   test_queries = [
     'show me one airbnb listing',
@@ -11,32 +10,41 @@ def main():
     'find a quiet place with good reviews'
   ]
 
+# test qdrant output for listing_id
+  client = QdrantClient(host='localhost', port=6333)
+  points = client.retrieve(
+    collection_name='airbnb_reviews',
+    ids=[5844519],
+    with_payload=True)
+  for point in points:
+    print(point.payload)
+
 # test qdrant output
-  print('running test harness for Qdrant + neo4j...')
+  # print('running test harness for Qdrant + neo4j...')
 
-  for query in test_queries:
-    print('\n==============================')
-    print(f"Query: {query}")
+  # for query in test_queries:
+  #   print('\n==============================')
+  #   print(f"Query: {query}")
 
-    docs = vector_store.similarity_search(query, k=1)
-    if not docs:
-      print('No matching docuents found in Qdrant')
-      continue
-    else:
-      for doc in docs:
-        print(f"Matched Doc Content: {doc.page_content}")
-        print(f"Metadata: {doc.metadata}")
+  #   docs = vector_store.similarity_search(query, k=1)
+  #   if not docs:
+  #     print('No matching docuents found in Qdrant')
+  #     continue
+  #   else:
+  #     for doc in docs:
+  #       print(f"Matched Doc Content: {doc.page_content}")
+  #       print(f"Metadata: {doc.metadata}")
 
-        listing_id = doc.metadata.get('listing_id')
-        if listing_id:
-          neo4j_meta = query_neo4j(str(listing_id))
-          print(f"neo4j data: {neo4j_meta}")
-        else:
-          print('no listing_id found in metadata')
+  #       listing_id = doc.metadata.get('listing_id')
+  #       if listing_id:
+  #         neo4j_meta = query_neo4j(str(listing_id))
+  #         print(f"neo4j data: {neo4j_meta}")
+  #       else:
+  #         print('no listing_id found in metadata')
 
 
 # test llm output
-  print('running test harness for llm output...')
+  # print('running test harness for llm output...')
     # result = run_agent(query)
     # print(f"Result:\n{result['answer']}")
 
